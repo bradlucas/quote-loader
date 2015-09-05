75d24d45126af4e06c5c0a5943fe281c9b007242
Brad Lucas <brad@beaconhill.com>
2015-09-04 23:11:38 -0400

    updated version mysql-connector-java

--- project.clj
+++ project.clj
@@ -2,6 +2,9 @@
   :description "quote-loader"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clojure-csv/clojure-csv "2.0.1"]
-                 [org.clojure/java.jdbc "0.4.1"]
-                 [mysql/mysql-connector-java "5.1.18"]]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 ;; TODO java.jdbc changed API after 0.2.3
+                 ;; latest is 
+                 ;; [org.clojure/java.jdbc "0.4.1"]
+                 [mysql/mysql-connector-java "5.1.36"]]
   :main quote-loader.core)
