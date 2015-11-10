55f6f5e6ba954a1a3fb413f652a2b2ec7a58b6f4
Brad Lucas <brad@beaconhill.com>
2015-11-09 22:16:56 -0500

    updated clojure version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.blucas/quote-loader "1.0.0"
   :description "quote-loader"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [clojure-csv/clojure-csv "1.3.2"]
                  [org.clojure/java.jdbc "0.1.1"]
                  [mysql/mysql-connector-java "5.1.18"]]
