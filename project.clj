6d3027c1c36a5161cf1f7be00529ca251aff6bba
Brad Lucas <brad@beaconhill.com>
2015-11-10 18:51:20 -0500

    update project to org.clojure/java.jdbc 0.4.2

--- project.clj
+++ project.clj
@@ -2,9 +2,6 @@
   :description "quote-loader"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clojure-csv/clojure-csv "2.0.1"]
-                 [org.clojure/java.jdbc "0.2.3"]
-                 ;; TODO java.jdbc changed API after 0.2.3
-                 ;; latest is 
-                 ;; [org.clojure/java.jdbc "0.4.1"]
+                 [org.clojure/java.jdbc "0.4.2"]
                  [mysql/mysql-connector-java "5.1.36"]]
   :main quote-loader.core)
