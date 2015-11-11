9394eed76bfa65e2ebf37b74e461ccfdc7dd4c8f
Brad Lucas <brad@beaconhill.com>
2015-11-10 22:45:17 -0500

    add jvm-opts to project

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
                  [clojure-csv/clojure-csv "2.0.1"]
                  [org.clojure/java.jdbc "0.4.2"]
                  [mysql/mysql-connector-java "5.1.36"]]
-  :main quote-loader.core)
+  :main quote-loader.core
+  ;; :jvm-opts ["-Xmx768M"]
+  :jvm-opts ["-Xmx1g"]
+  )
