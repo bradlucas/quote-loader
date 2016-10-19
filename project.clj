9a0dbbd913d1310548b23cdfaf51919a77e295c6
Brad Lucas <brad@beaconhill.com>
2016-10-19 17:00:50 -0400

    version 1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.blucas/quote-loader "1.0.0"
+(defproject org.clojars.blucas/quote-loader "1.0.1"
   :description "quote-loader"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clojure-csv/clojure-csv "2.0.1"]
